812d78d1a96c089ce45e86ba3b2ebf294e170ce5
RafatAiub <tanvirmahtab1996@gmail.com>
2022-03-02 16:39:26 +0600

    message added successfully^_^

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.getElementById('error').style.display = 'none';
+
 const search = () => {
 
 
     const searchField = document.getElementById('search-field');
     const searchText = (searchField.value);
     if (searchText == '') {
+        document.getElementById('not-found').style.display = 'none';
         //show message
+        document.getElementById('blank').style.display = 'block';
     }
     // clear data 
     searchField.value = "";
@@ -22,29 +25,41 @@ const displayError = error => {
 }
 
 const displayphones = phones => {
+    // initailly display none 
+    document.getElementById('not-found').style.display = 'none';
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
     // searchResult.innerHTML = '';
     searchResult.textContent = '';
-    // if (phones.length == 0) {
-    //     //show no result found 
-    // }
+    console.log(phones.length);
+    if (phones.length == 0) {
+        //show no result found 
+        const notFound = document.getElementById('not-found');
+        notFound.style.display = 'block';
+    }
+
     phones.forEach(phone => {
+
         // console.log(phone);
+
         const div = document.createElement('div');
         div.classList.add('col');
+
+        //only 20 search item shows 
+
         div.innerHTML = `
-        <div  class="card">
+        <div  class="card p-3">
+            
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <h6 class="card-text">${phone.brand}</h6>
             </div>
-            <button onclick="loadphoneDetail('${phone.slug}')">Details</button>
+            <button class="btn-outline-info" onclick="loadphoneDetail('${phone.slug}')">Details</button>
         </div>
             `;
-
         searchResult.appendChild(div);
+
     })
 }
 
@@ -70,14 +85,16 @@ const displayphoneDetail = phone => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top" alt="...">
-    <div class="card-body">
+    <div class="row p-3 pb-5 ">
+    <div class=" col-12 col-md-7 g-3 mx-auto"><img src="${phone.image}" class="card-img-top" alt="..."></div>
+    
+    <div class="col-12 card-body col-md-5">
         <h4 class="card-title">${phone.name}</h4>
         <p class="card-text text-primary">${phone.releaseDate}</p>
         <p class="card-text">${phone.mainFeatures.chipSet}[chip-set]</p>
         
         
-    </div>
+    </div></div>
     `;
     phoneDetails.appendChild(div);
 }
